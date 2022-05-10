de9b1b8fe16ed47f48398b6d5015e8c46306989c
Agustín García <agustin.tavoite@gmail.com>
2022-05-10 17:56:00 +0200

    createElement

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,9 @@
 
 const gameContainer = document.getElementById ("gameContainer");
 const gridContainer = ['','','',''];
+
+gridContainer.map(function(){
+
+let rowContainer = document.createElement("div")
+gameContainer.appendChild(rowContainer);
+});
