4907c31aa886b0ae638c6afc7e346cb2801f61e6
Ricardo Vieira <vieiraraba@gmail.com>
2022-05-11 19:22:52 +0200

    feat(dragonBall): create element inside the array and set src attribute to an image

--- src/js/main.js
+++ src/js/main.js
@@ -4,16 +4,21 @@ const gridContainer = [
     ["", "", "", ""],
     ["", "", "", ""]
 ];
+const dragonBall = "https://i.pinimg.com/originals/6b/93/91/6b9391fc5f7e7354d1067116b5b1d372.png"
 
 let index = 0;
 gridContainer.map(function (row) {
     let rowContainer = document.createElement("div");
     rowContainer.setAttribute("id", "balloon" + index++);
     rowContainer.classList.add("purple-balloon");
-    console.log("hola");
 
     row.map(function () {
+      let redBalloon = document.createElement("img");
+      redBalloon.src = dragonBall;
+      redBalloon.classList.add("dragon-ball");
 
+
+      rowContainer.appendChild(redBalloon)
     });
 
 
